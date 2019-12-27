7d5c2c553819229a3a89ddc1fc86c95095492417
arditian <45433379+arditian@users.noreply.github.com>
2019-12-27 13:42:04 +0700

    update again

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sub1ardit.database.windows.net,1433; Database = sub1database", "ardit", "submission1!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ardit", "pwd" => "submission1!", "Database" => "sub1database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sub1ardit.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
-
 <html>
  <head>
  <Title>Registration Form</Title>
