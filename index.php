f2b29631b970782dec029146f80358f067891f3f
arditian <45433379+arditian@users.noreply.github.com>
2019-12-27 13:32:54 +0700

    update database connection

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sub1ardit.database.windows.net,1433; Database = sub1database", "ardit", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:sub1ardit.database.windows.net,1433; Database = sub1database", "ardit", "submission1!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ardit", "pwd" => "{your_password_here}", "Database" => "sub1database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ardit", "pwd" => "submission1!", "Database" => "sub1database", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:sub1ardit.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
